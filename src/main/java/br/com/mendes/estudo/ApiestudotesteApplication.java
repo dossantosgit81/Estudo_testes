d99c84d75db5b64736c664545125fc06b28f9701
Rafael Mendes dos Santos <dossantosgit81@gmail.com>
2022-05-04 23:36:49 -0300

    Projeto iniciado

---
+++ src/main/java/br/com/mendes/estudo/ApiestudotesteApplication.java
@@ -0,0 +1,13 @@
+package br.com.mendes.estudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiestudotesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiestudotesteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mendes/estudo/ApiestudotesteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mendes.estudo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiestudotesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
