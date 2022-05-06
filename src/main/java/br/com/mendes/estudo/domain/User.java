c1d49ea264a0b9c3b8481d74db3238c8bb6b52a5
Rafael Mendes dos Santos <dossantosgit81@gmail.com>
2022-05-05 23:42:50 -0300

    Adiciona identificador no user, cria userResource e seu primeiro endpoint

--- src/main/java/br/com/mendes/estudo/domain/User.java
+++ src/main/java/br/com/mendes/estudo/domain/User.java
@@ -5,6 +5,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @AllArgsConstructor
@@ -12,6 +15,8 @@ import javax.persistence.Entity;
 @Entity
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String email;
--- src/main/java/br/com/mendes/estudo/resources/UserResource.java
+++ src/main/java/br/com/mendes/estudo/resources/UserResource.java
@@ -2,6 +2,7 @@ package br.com.mendes.estudo.resources;
 
 import br.com.mendes.estudo.domain.User;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value="/user")
 public class UserResource {
 
+    @GetMapping(value="/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id){
 
         return ResponseEntity.ok().body(new User(1, "Rafael", "rafael@gmail.com", "123"));
