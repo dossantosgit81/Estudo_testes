3e6de25d5ccbbfbac42abf84cdad394fb516c7cc
Rafael Mendes dos Santos <dossantosgit81@gmail.com>
2022-05-08 15:34:41 -0300

    Teste classe userserviceImpl delete sucecess

--- src/test/java/br/com/mendes/estudo/services/impl/UserServiceImplTest.java
+++ src/test/java/br/com/mendes/estudo/services/impl/UserServiceImplTest.java
@@ -20,7 +20,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class UserServiceImplTest {
@@ -149,7 +149,11 @@ class UserServiceImplTest {
 
 
     @Test
-    void delete() {
+    void deleteWithSucess() {
+        when(repository.findById(anyInt())).thenReturn(optionalUser);
+        doNothing().when(repository).deleteById(anyInt());
+        service.delete(ID);
+        verify(repository, times(1)).deleteById(anyInt());
     }
 
     private void startUser() {
